1e42dcf675020433d95b22ca8e58d4a056ae11a2
Pietro Ruotolo <pietroschimidt03082007@gmail.com>
2026-04-19 13:54:33 -0300

    chore: estruturando a base do projeto

---
+++ src/js/script.js
@@ -0,0 +1,37 @@
+let nome = prompt("Insira seu nome: ");
+let estado = prompt("Insira o seu estado: ");
+alert(`Cadastro realizado!\nSeja bem vindo ${nome}, Veja os detalhes no console.`);
+
+console.log(`Olá ${nome}, esses são os nossos vinhos disponíveis para o estado de ${estado}: `);
+
+console.log(`Cabernet 2020:
+TIPO - Tinto
+
+SAFRA - 2020
+
+ESTOQUE - 15  
+
+PAÍS - França 
+`);
+
+
+console.log(`Sauvignon Blanc 2023:
+TIPO - Branco
+
+SAFRA - 2023
+
+ESTOQUE - 30 
+
+PAÍS - Argentina 
+`);
+
+
+console.log(`Viejo Feo Reserva Pinot Noir Rosé 2024:
+TIPO - Rosé
+
+SAFRA - 2024
+
+ESTOQUE - 50 
+
+PAÍS - Chile`
+);
